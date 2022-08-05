3f01b5c999ed6fcae1dec5969e433fffd42d3526
Zoë Hoekstra <altlockg@gmail.com>
2022-08-05 15:05:43 +0200

    Add working webpage for configuration

--- src/main/webServer.go
+++ src/main/webServer.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -13,10 +14,33 @@ func webServer() {
 		fmt.Fprint(w, index)
 	})
 
-	http.HandleFunc("/set", func(w http.ResponseWriter, r *http.Request) {
-		if r.PostForm.Has("x") {
-
+	http.HandleFunc("/getSettings", func(w http.ResponseWriter, r *http.Request) {
+		val, err := json.Marshal(configuration)
+		if err != nil {
+			return
 		}
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		fmt.Fprint(w, string(val))
+	})
+
+	http.HandleFunc("/setSettings", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			return
+		}
+		if r.ParseForm() != nil {
+			return
+		}
+		settingsString := r.PostForm.Get("settings")
+		conf := Configuration{}
+
+		err := json.Unmarshal([]byte(settingsString), &conf)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		configuration = conf
+		fmt.Println("Applied settings; ", configuration)
 	})
 
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(configuration.WebPort), nil))
